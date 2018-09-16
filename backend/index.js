aa07e6777e25b4eac0bc666bce5743b305b1a498
albertopardo21690 <albertopardo21690@gmail.com>
2018-09-16 19:41:16 +0200

    Cambie la variable "var port = process.env.PORT || 1990;" a "var port = normalizePort(process.env.PORT || 1990);"

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 49672;
+var port = normalizePort(process.env.PORT || 1990);
 
 mongoose.connect('mongodb://albertopardo21690:Titanic1914@ds149672.mlab.com:49672/centro_multimedia', (err, res) => {
     if(err){
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://albertopardo21690:Titanic1914@ds149672.mlab.com:4967
         console.log("La conexión a la base de datos está funcionando correctamente...");
 
         app.listen(port, function(){
-            console.log("El servidor del API Rest del Centro Multimedia escuchando en http://ds149672.mlab.com:" + port);
+            console.log("El servidor del API Rest del Centro Multimedia escuchando en http://localhost:" + port);
             
         });
     }
